dd319e8fe3a6b8a9463baf386bd77449038e1ba8
EcosDigital <tu_email@ejemplo.com>
2025-02-01 14:47:39 -0500

    update shema categoria

--- src/schema/config/categoriaPost.js
+++ src/schema/config/categoriaPost.js
@@ -3,11 +3,11 @@ import { z } from "zod";
 //schema para validar la creacion de una nueva categoria
 export const categoriaSchema = z.object({
   id_area: z
-    .number({ message: "Debes anexar el id area" })
+    .number()
     .int({ message: "Debe ser un numero positivo" })
     .positive({ message: "Debe ser un numero positivo" }),
 
   name: z
-    .string({ message: "Debe ser un string" })
+    .string()
     .min(5, { message: "Debe contener almenos 5 caracteres" }),
 });
