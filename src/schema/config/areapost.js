2de7363e425decac56338d2461987ba9c99689dc
EcosDigital <tu_email@ejemplo.com>
2025-02-01 14:32:12 -0500

    update max caracteres for area

--- src/schema/config/areapost.js
+++ src/schema/config/areapost.js
@@ -2,5 +2,5 @@ import { z } from "zod";
 
 //sechema que valida la creacion de una nueva area
 export const validateSchemaNewArea = z.object({
-    name: z.string({message : "Debe ser un string"}).min(5,{message :"Debe contener almenos 5 caracteres"})
+    name: z.string({message : "Debe ser un string"}).min(3,{message :"Debe contener almenos 5 caracteres"})
 })
